655dca4303ba102da976f01764c0a8f89267e4c6
Pete K <krautscheid@mitre.org>
2024-06-12 14:05:48 -0400

    Update wording in python script

--- proportion_with_unsuitable_underlying.py
+++ proportion_with_unsuitable_underlying.py
@@ -9,14 +9,14 @@ unsuitable_causes = pd.read_csv("unsuitable_COD_codes.csv")
 # Extract the unsuitable codes
 unsuitable_codes = unsuitable_causes["code"].values
 
-# Determine the number of records with an unsuitable cause of death
+# Determine the number of records with an unsuitable underlying cause of death
 unsuitable_cod_records = death_records[
     death_records["Underlying COD"].isin(unsuitable_codes)
 ]
 
-# Calculate the proportion of records with an unsuitable cause of death
+# Calculate the proportion of records with an unsuitable underlying cause of death
 proportion = len(unsuitable_cod_records) / len(death_records)
 
 print(
-    f"The proportion of records with an unsuitable cause of death is {proportion:.2f}"
+    f"The proportion of records with an unsuitable underlying cause of death is {proportion:.2f}"
 )
