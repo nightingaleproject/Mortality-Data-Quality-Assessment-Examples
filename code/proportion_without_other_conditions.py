8bb4e8dad54a49bb96cffa6fbc58a9e6f104b131
Pete K <krautscheid@mitre.org>
2024-09-17 13:19:43 -0400

    Make "other conditions" example consistent with other examples

--- code/proportion_without_other_conditions.py
+++ code/proportion_without_other_conditions.py
@@ -13,22 +13,20 @@ death_records = pd.read_csv(
 
 # Find all 'Entity Axis Line' columns
 entity_axis_line_cols = [
-    col for col in death_records.columns if "Entity Axis Line" in col
+    col for col in death_records.columns if col.startswith("Entity Axis Line")
 ]
 
 # Records where the certifier provided "other conditions" in the
 # literal cause of death information will refer to those in the entity
 # axis codes as "line 6"; create a boolean mask for records that do
 # not contain a 6 in any of the 'Entity Axis Line' columns
-records_without_other_conditions = ~death_records[entity_axis_line_cols].apply(
-    lambda x: x.isin([6]).any(), axis=1
+death_records["No Other Conditions"] = death_records[entity_axis_line_cols].apply(
+    lambda row: (row != 6).all(), axis=1
 )
 
 # Calculate the proportion of records that do not refer to other conditions
-proportion_no_other_conditions = records_without_other_conditions.sum() / len(
-    death_records
-)
+proportion = death_records["No Other Conditions"].mean()
 
 print(
-    f"The proportion of records that do not refer to other conditions in the entity axis codes is {proportion_no_other_conditions:.2f}"
+    f"The proportion of records that do not refer to other conditions in the entity axis codes is {proportion:.2f}"
 )
