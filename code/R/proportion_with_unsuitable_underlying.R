48d0080ccd9f0682dfc76a899fa3da08125bae3c
Dr. Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2025-05-05 15:10:21 -0400

    Switch to here and remove dplyr dependency

--- code/R/proportion_with_unsuitable_underlying.R
+++ code/R/proportion_with_unsuitable_underlying.R
@@ -1,8 +1,8 @@
-# Load dplyr library for data manipulation
-library(dplyr)
+# Load necessary libraries
+library(here)
 
-# Locate the relative path to the data location
-data_path <- file.path(dirname(normalizePath(sys.frame(1)$ofile)), "..", "..", "data")
+# Specify the relative path to the data location
+data_path <- here::here("data")
 
 # Load the death records data
 death_records <- read.csv(
@@ -18,14 +18,13 @@ unsuitable_causes <- read.csv(file.path(data_path, "unsuitable_COD_codes.csv"))
 # Extract the unsuitable codes
 unsuitable_codes <- unsuitable_causes$code
 
-# Vectorized function to check if any unsuitable code is a prefix to the code in the record
-is_unsuitable <- Vectorize(function(code) {
-  any(startsWith(code, unsuitable_codes))
-})
-
 # Create a new column that is TRUE when the underlying COD is unsuitable
-death_records <- death_records |>
-  mutate(`Unsuitable Underlying` = is_unsuitable(`Underlying COD`))
+death_records[, "Unsuitable Underlying"] <- sapply(
+  death_records$`Underlying COD`, 
+  function(code){
+    return(any(startsWith(code, unsuitable_codes)))
+  }
+)
 
 # Calculate the proportion of records with an unsuitable underlying cause of death
 proportion <- mean(death_records$`Unsuitable Underlying`, na.rm = TRUE)
@@ -35,9 +34,12 @@ cat(paste("The proportion of records with an unsuitable underlying cause of deat
           "\n"))
 
 # Group the records by certifier and calculate the proportion of unsuitable records for each certifier
-certifier_proportions <- death_records |>
-  group_by(`Certifier Name`) |>
-  summarize(Proportion = mean(`Unsuitable Underlying`, na.rm = TRUE))
+certifier_proportions <- 
+  aggregate(
+    list("Proportion" = death_records$`Unsuitable Underlying`),
+    list("Certifier Name" = death_records$`Certifier Name`),
+    FUN = mean,
+    na.rm = TRUE)
 
 # Print the proportions for each certifier
 for(i in 1:nrow(certifier_proportions)) {
