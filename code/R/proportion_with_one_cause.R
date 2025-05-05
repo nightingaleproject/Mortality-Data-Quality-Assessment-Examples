b6682d451f77c53e3cabf41964fcfc10adbbc844
Dr. Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2025-05-05 14:49:10 -0400

    Update one cause to use here and remove dplyr dependency

--- code/R/proportion_with_one_cause.R
+++ code/R/proportion_with_one_cause.R
@@ -1,8 +1,8 @@
-# Load necessary library
-library(dplyr)
+# Load necessary libraries
+library(here)
 
-# Locate the relative path to the data location
-data_path <- file.path(dirname(normalizePath(sys.frame(1)$ofile)), "..", "..", "data")
+# Specify the relative path to the data location
+data_path <- here::here("data")
 
 # Load the death records data
 death_records <- read.csv(
@@ -13,8 +13,10 @@ death_records <- read.csv(
 )
 
 # Create a new column that is True when a single Record Axis COD column is populated
-death_records <- death_records %>%
-  mutate(`Single Record Axis COD` = rowSums(!is.na(select(., starts_with("Record Axis COD")))) == 1)
+death_records[, "Single Record Axis COD"] <-
+  rowSums(!is.na(
+    death_records[, startsWith(colnames(death_records), "Record Axis COD")])
+  ) == 1
 
 # Calculate the proportion of records with only one Record Axis COD
 proportion <- mean(death_records$`Single Record Axis COD`, na.rm = TRUE)
@@ -23,9 +25,12 @@ cat(paste("The proportion of records with a single Record Axis COD is",
           round(proportion, 2), "\n"))
 
 # Group the records by certifier and calculate the proportion of flagged records for each certifier
-certifier_proportions <- death_records %>%
-  group_by(`Certifier Name`) %>%
-  summarize(Proportion = mean(`Single Record Axis COD`, na.rm = TRUE))
+certifier_proportions <- 
+  aggregate(
+    list("Proportion" = death_records$`Single Record Axis COD`),
+    list("Certifier Name" = death_records$`Certifier Name`),
+    FUN = mean,
+    na.rm = TRUE)
 
 # Print the proportions for each certifier
 for(i in 1:nrow(certifier_proportions)) {
